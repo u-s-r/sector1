307c6776c614a69a3c35953fb84de8d6200bf274
Cătălin Dogaru <catalin.dogaru@gmail.com>
2017-03-31 13:33:03 +0300

    Added carousel

--- js/main.js
+++ js/main.js
@@ -2,4 +2,26 @@
 	'use strict';
 
 	$( '.context-height .eq-height' ).matchHeight();
+
+	$( '.carousel' ).slick({
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToScroll: 1,
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToScroll: 2,
+					slidesToShow: 2
+				}
+			}
+		],
+		slidesToScroll: 3,
+		slidesToShow: 3
+	});
 })( jQuery );
